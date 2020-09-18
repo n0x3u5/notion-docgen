cd5dfda35d04cac6fa52f118d3731cc788376e33
Nakshatra Mukhopadhyay <nakshatra.mukhopadhyay1994@gmail.com>
2020-09-18 19:03:24 +0530

    The root folder of the generated documentation now has a constant name

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@
 import { stat, readdir, readFile, writeFile, ensureFile, copy } from 'fs-extra';
 import type { Stats } from 'fs-extra';
 import { URL } from 'url';
-import { resolve, extname, basename, sep, parse } from 'path';
+import { resolve, extname, basename, sep, parse, relative } from 'path';
 import slugify from 'slugify';
 import remark from 'remark';
 import toc from 'mdast-util-toc';
@@ -53,6 +53,7 @@ interface Ret {
   toc?: [string, Parent][];
 }
 
+const DOCS_ROOT_NAME = 'public-documentation';
 const sourceDir = resolve('notion-md-export');
 const outDir = resolve('docs');
 const sidebarFile = resolve(outDir, '_sidebar.md');
@@ -164,10 +165,10 @@ function normalizePathToken(pathToken: string, idx: number, arr: string[]) {
 }
 
 function normalizePath(path: string): string {
-  return path
-    .substring(sourceDir.length + 1)
-    .split(sep)
-    .map(normalizePathToken)
+  return [DOCS_ROOT_NAME]
+    .concat(
+      relative(sourceDir, path).split(sep).slice(1).map(normalizePathToken)
+    )
     .join(sep);
 }
 
@@ -190,7 +191,7 @@ function ensureFiles(files: string[]) {
   );
 }
 
-async function y(srcDirectory: string): Promise<Ret> {
+async function scrape(srcDirectory: string): Promise<Ret> {
   const dirItems = await readDir(srcDirectory);
 
   const { files, dirs: subDirs } = groupByIsDirectory(dirItems);
@@ -217,6 +218,10 @@ async function y(srcDirectory: string): Promise<Ret> {
     ensureFiles(nonToCMarkdownItems.map(({ path }) => path)),
     ensureFiles(nonToCNonMarkdownItems.map(({ path }) => path)),
   ]);
+
+  nonToCNonMarkdownItems.forEach((item) =>
+    copy(item.path, resolve(outDir, normalizePath(item.path)))
+  );
 
   nonToCMarkdownItemContents.forEach(({ content, path }) => {
     const r = remark();
@@ -277,7 +282,7 @@ async function y(srcDirectory: string): Promise<Ret> {
       docFileMap: nonToCMarkdownFileMapEntries,
     };
   } else {
-    const rets = await Promise.all(subDirs.map(({ path }) => y(path)));
+    const rets = await Promise.all(subDirs.map(({ path }) => scrape(path)));
 
     const x = rets
       .map(({ toc }) => toc)
@@ -360,8 +365,8 @@ async function y(srcDirectory: string): Promise<Ret> {
   }
 }
 
-(async function x() {
-  const { toc: sideBarToC, docFileMap: filesMap } = await y(sourceDir);
+(async () => {
+  const { toc: sideBarToC, docFileMap: filesMap } = await scrape(sourceDir);
 
   const fileHashMap = Object.fromEntries(filesMap);
 
