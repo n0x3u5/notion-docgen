7e63c5aef68489eaa0addec6e13bf5f34a8a801c
Nakshatra Mukhopadhyay <nakshatra.mukhopadhyay1994@gmail.com>
2020-10-05 02:31:41 +0530

    Generate build for usage via NPM via Git URL

--- index.js
+++ index.js
@@ -15,11 +15,17 @@ const unist_util_visit_1 = __importDefault(require("unist-util-visit"));
 const mdast_builder_1 = require("mdast-builder");
 const process_1 = require("process");
 const yargs_parser_1 = __importDefault(require("yargs-parser"));
+const winston_1 = require("winston");
 const DOCS_ROOT_NAME = 'public-documentation';
+const WARNING_SEPARATOR = '=====================================================================';
 const yargv = yargs_parser_1.default(process_1.argv);
 const sourceDir = path_1.resolve(typeof yargv.src === 'string' ? yargv.src : 'notion-md-export');
 const outDir = path_1.resolve(typeof yargv.out === 'string' ? yargv.out : 'docs');
 const sidebarFile = path_1.resolve(outDir, '_sidebar.md');
+const logger = winston_1.createLogger({
+    format: winston_1.format.combine(winston_1.format.errors({ stack: true }), winston_1.format.colorize(), winston_1.format.simple()),
+    transports: new winston_1.transports.Console(),
+});
 const trimLastWord = (s) => s.split(' ').slice(0, -1).join(' ');
 function groupByIsToCItem(files, contentfulSubDirsContents) {
     return files.reduce((acc, item) => {
@@ -115,6 +121,28 @@ function isPen(p) {
         p.includes('/professor/') ||
         p.includes('/pres/'));
 }
+function getLinkText(linkNode) {
+    let linkText = '';
+    unist_util_visit_1.default(linkNode, 'text', ({ value }) => {
+        if (typeof value === 'string')
+            linkText = value;
+    });
+    return linkText;
+}
+function toAbsolute(relativePath, absoluteDirPath) {
+    const absoluteDirPathParts = path_1.dirname(absoluteDirPath).split(path_1.sep);
+    const relativePathParts = relativePath.split(path_1.sep);
+    let dblDotCount = 0;
+    relativePathParts.every((part) => {
+        const isDblDot = part == '..';
+        if (isDblDot)
+            dblDotCount++;
+        return isDblDot;
+    });
+    return absoluteDirPathParts
+        .concat(relativePathParts.slice(dblDotCount))
+        .join(path_1.sep);
+}
 async function scrape(srcDirectory) {
     const dirItems = await readDir(srcDirectory);
     const { files, dirs: subDirs } = groupByIsDirectory(dirItems);
@@ -132,11 +160,16 @@ async function scrape(srcDirectory) {
     nonToCMarkdownItemContents.forEach(({ content, path }) => {
         const r = remark_1.default();
         const tree = r.parse(content);
+        const outPath = path_1.resolve(outDir, normalizePath(path));
         const t = unist_util_map_1.default(tree, (node) => {
+            var _a, _b, _c, _d;
             if (node.type === 'text' &&
                 typeof node.value === 'string' &&
                 node.value.toLowerCase().includes('table of content')) {
                 const tocTree = mdast_util_toc_1.default(tree, { tight: true });
+                logger.warn(`File: ${outPath}:${(_b = (_a = node.position) === null || _a === void 0 ? void 0 : _a.start) === null || _b === void 0 ? void 0 : _b.line}:${(_d = (_c = node.position) === null || _c === void 0 ? void 0 : _c.start) === null || _d === void 0 ? void 0 : _d.column}`);
+                logger.warn('A stray "Table of Contents" string was found. Check the Notion page for the file above.');
+                logger.warn(WARNING_SEPARATOR);
                 return tocTree.map == null
                     ? node
                     : mdast_builder_1.paragraph([mdast_builder_1.strong(mdast_builder_1.text('Table of Contents')), mdast_builder_1.brk, tocTree.map]);
@@ -167,7 +200,7 @@ async function scrape(srcDirectory) {
                 return node;
             }
         });
-        fs_extra_1.writeFile(path_1.resolve(outDir, normalizePath(path)), r.stringify(t));
+        fs_extra_1.writeFile(outPath, r.stringify(t));
     });
     const nonToCMarkdownFileMapEntries = nonToCMarkdownItems.map(({ path }) => {
         const { name } = path_1.parse(path);
@@ -254,25 +287,58 @@ async function scrape(srcDirectory) {
     const r = remark_1.default();
     fileContents.forEach(({ content, path }) => {
         const t = unist_util_map_1.default(r.parse(content), (node) => {
+            var _a, _b, _c, _d, _e, _f, _g, _h;
             if (node.type === 'link' && typeof node.url === 'string') {
                 try {
                     const { hostname, pathname } = new url_1.URL(node.url);
                     const pathnameParts = pathname.split('/');
-                    if (hostname.includes('notion.so') &&
-                        fileHashMap[pathnameParts[pathnameParts.length - 1]]) {
-                        let linkText = '';
-                        unist_util_visit_1.default(node, 'text', (node) => {
-                            if (typeof node.value === 'string')
-                                linkText = node.value;
-                        });
-                        return mdast_builder_1.link(fileHashMap[pathnameParts[pathnameParts.length - 1]].substring(outDir.length + 1), linkText);
+                    if (hostname.includes('notion.so')) {
+                        if (fileHashMap[pathnameParts[pathnameParts.length - 1]]) {
+                            return mdast_builder_1.link(fileHashMap[pathnameParts[pathnameParts.length - 1]].substring(outDir.length + 1), getLinkText(node));
+                        }
+                        else {
+                            logger.warn(`File: ${path}:${(_b = (_a = node.position) === null || _a === void 0 ? void 0 : _a.start) === null || _b === void 0 ? void 0 : _b.line}:${(_d = (_c = node.position) === null || _c === void 0 ? void 0 : _c.start) === null || _d === void 0 ? void 0 : _d.column}`);
+                            logger.warn(`No matching local file found for link with the URL "${node.url}" and text "${getLinkText(node)}". Check the Notion page for the file above.`);
+                            logger.warn(WARNING_SEPARATOR);
+                            return node;
+                        }
                     }
                     else {
                         return node;
                     }
                 }
                 catch {
-                    return node;
+                    if (path_1.extname(node.url) === '.md') {
+                        const normalizedRelativePath = decodeURI(node.url)
+                            .split(path_1.sep)
+                            .map((part, idx, arr) => {
+                            if (part === '..' || part === '.') {
+                                return part;
+                            }
+                            else {
+                                return normalizePathToken(part, idx, arr);
+                            }
+                        })
+                            .join(path_1.sep);
+                        return mdast_builder_1.link(toAbsolute(normalizedRelativePath, path).substr(outDir.length), getLinkText(node));
+                    }
+                    else {
+                        const linkText = getLinkText(node);
+                        if (node.url === '' || linkText === '') {
+                            logger.warn(`File: ${path}:${(_f = (_e = node.position) === null || _e === void 0 ? void 0 : _e.start) === null || _f === void 0 ? void 0 : _f.line}:${(_h = (_g = node.position) === null || _g === void 0 ? void 0 : _g.start) === null || _h === void 0 ? void 0 : _h.column}`);
+                            if (node.url === '' && linkText === '') {
+                                logger.warn('Both the URL and the text of a link was found to be blank. Check the Notion page for the file above.');
+                            }
+                            else if (node.url === '') {
+                                logger.warn(`The URL of a link with text "${linkText}" was found to be blank. Check the Notion page for the file above.`);
+                            }
+                            else if (linkText === '') {
+                                logger.warn(`The text of a link with URL "${node.url}" was found to be blank. Check the Notion page for the file above.`);
+                            }
+                            logger.warn(WARNING_SEPARATOR);
+                        }
+                        return node;
+                    }
                 }
             }
             else {
